f26d1e3754d510a387bb5529cbbc69bdd2d91a1d
Viktar <1105714@gmail.com>
2023-11-09 17:32:10 +0400

    core-api:  - polish schemas

--- packages/core-api/schemas/SyngrisiApi.schema.ts
+++ packages/core-api/schemas/SyngrisiApi.schema.ts
@@ -1,7 +1,10 @@
 import { z } from 'zod'
 
+const viewPort = z.string().min(3).regex(/^\d+x\d+$/) // regex for 'width x height' format
+const idString = z.string().length(24) // Assuming all ID-like strings are MongoDB ObjectIDs with fixed 24-character length
+
 export const SnapshotSchema = z.object({
-    _id: z.string().length(24).optional(),
+    _id: idString.optional(),
     name: z.string().min(1).optional(),
     filename: z.string().min(1).optional(),
     imghash: z.string().length(128).optional(),
@@ -27,7 +30,7 @@ export type SnapshotResponse = z.infer<typeof SnapshotResponseSchema>;
 
 export const BaselineParamsSchema = z.object({
     name: z.string().min(1),
-    viewport: z.string().regex(/^\d+x\d+$/).min(1),
+    viewport: viewPort,
     browserName: z.string().min(1),
     os: z.string().min(1),
     app: z.string().min(1),
@@ -39,9 +42,7 @@ export const BaselineResponseSchema = getResponseSchema(BaselineParamsSchema)
 
 export type BaselineResponse = z.infer<typeof BaselineResponseSchema>;
 
-const idString = z.string().length(24) // Assuming all ID-like strings are MongoDB ObjectIDs with fixed 24-character length
-const viewportRegex = z.string().regex(/^\d{3,4}x\d{3,4}$/) // Validates viewport strings like '500x500', '1024x768' etc.
-const statusSchema = z.union([
+const checkStatusSchema = z.union([
     z.string().min(1),
     z.array(z.string().min(1)),
 ])
@@ -55,14 +56,14 @@ export const CheckResponseSchema = z.object({
     baselineId: idString,
     actualSnapshotId: idString,
     updatedDate: z.string().min(1), // This assumes ISO date string format
-    status: statusSchema,
+    status: checkStatusSchema,
     browserName: z.string().min(1),
     browserVersion: z.union([
         z.string().min(1),
         z.number(),
     ]),
     browserFullVersion: z.string().min(1),
-    viewport: viewportRegex,
+    viewport: viewPort,
     os: z.string().min(1),
     result: z.string().min(1), // Adjusted if a JSON object is needed
     run: idString,
@@ -81,13 +82,13 @@ export type CheckResponse = z.infer<typeof CheckResponseSchema>;
 export const CheckParamsSchema = z.object({
     // ident:  ['name', 'viewport', 'browserName', 'os', 'app', 'branch'];
     name: z.string().min(1),
-    viewport: z.string().min(3),
+    viewport: viewPort,
     browserName: z.string().min(1),
     os: z.string().min(1),
     app: z.string().min(1),
     branch: z.string().min(1),
 
-    testId: z.string().min(1),
+    testId: idString,
     suite: z.string().min(1),
     browserVersion: z.union([
         z.string().min(1),
@@ -102,22 +103,22 @@ export const CheckParamsSchema = z.object({
 export type CheckParams = z.infer<typeof CheckParamsSchema>;
 
 export const ApiSessionParamsSchema = z.object({
-    run: z.string(),
-    suite: z.string(),
-    runident: z.string(),
-    name: z.string(),
-    viewport: z.string(),
-    browser: z.string(),
+    run: z.string().min(1),
+    suite: z.string().min(1),
+    runident: z.string().min(1),
+    name: z.string().min(1),
+    viewport: viewPort,
+    browser: z.string().min(1),
     browserVersion: z.union([
         z.string().min(1),
         z.number(),
     ]),
+    browserFullVersion: z.string().min(1),
     os: z.string(),
     app: z.string(),
     tags: z.array(z.string()).optional(),
-    branch: z.string().optional(),
+    branch: z.string(),
 })
-// .passthrough() // This method allows for unknown keys
 
 export type ApiSessionParams = z.infer<typeof ApiSessionParamsSchema>;
 
@@ -126,7 +127,7 @@ export const ApiSessionOptsSchema = z.object({
     suite: z.string(),
     runident: z.string(),
     name: z.string(),
-    viewport: z.string(),
+    viewport: viewPort,
     browser: z.string(),
     browserVersion: z.union([
         z.string().min(1),
@@ -135,7 +136,8 @@ export const ApiSessionOptsSchema = z.object({
     os: z.string(),
     app: z.string(),
     tags: z.array(z.string()).optional(),
-    branch: z.string().optional(),
+    branch: z.string(),
+    browserFullVersion: z.string().optional(),
 })
 
 export type ApiSessionOpts = z.infer<typeof ApiSessionOptsSchema>;
@@ -163,17 +165,17 @@ export const SessionResponseSchema = z.object({
     browserVersion: z.string().min(1),
     branch: z.string().min(1),
     tags: z.array(z.string()).min(1), // Assuming that an empty array is not allowed
-    viewport: z.string().min(1).regex(/^\d+x\d+$/), // regex for 'width x height' format
+    viewport: viewPort,
     os: z.string().min(1),
-    app: z.string().length(24), // Fixed length string for IDs
+    app: idString, // Fixed length string for IDs
     blinking: z.number(),
     updatedDate: z.string().min(1), // You may want to refine this to a date-time string format
     startDate: z.string().min(1), // Similar to updatedDate
     checks: z.array(z.array(z.string())), // Placeholder for any type as specifics are not given
-    suite: z.string().length(24), // Fixed length string for IDs
-    run: z.string().length(24), // Fixed length string for IDs
-    _id: z.string().length(24), // Fixed length string for IDs
-    id: z.string().length(24), // Fixed length string for IDs
+    suite: idString, // Fixed length string for IDs
+    run: idString, // Fixed length string for IDs
+    _id: idString, // Fixed length string for IDs
+    id: idString, // Fixed length string for IDs
 })
 
 export type SessionResponse = z.infer<typeof SessionResponseSchema>;
