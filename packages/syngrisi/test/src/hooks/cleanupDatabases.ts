50d4d7e2c764ce922c2923df36e57ac705e3be38
Viktor Silakov <23633060+viktor-silakov@users.noreply.github.com>
2024-05-23 16:42:33 +0400

    fiz eslint errors

--- packages/syngrisi/test/src/hooks/cleanupDatabases.ts
+++ packages/syngrisi/test/src/hooks/cleanupDatabases.ts
@@ -8,15 +8,19 @@ async function deleteTestDatabases() {
     const admin = connection.db.admin();
     const { databases } = await admin.listDatabases();
 
-    const testDatabases = databases.filter((db: any) => db.name.startsWith('testdb_'));
+    const testDatabases = databases.filter((db) => db.name.startsWith('testdb_'));
 
     for (const db of testDatabases) {
       const dbConnection = connection.useDb(db.name, { useCache: false });
       await dbConnection.dropDatabase();
       console.log(`Dropped database: ${db.name}`);
     }
-  } catch (error) {
-    console.error(`Failed to delete test databases: ${error.message}`);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(`Failed to delete test databases: ${error.message}`);
+    } else {
+      console.error(`Failed to delete test databases: ${error}`);
+    }
   } finally {
     await connection.close();
   }
