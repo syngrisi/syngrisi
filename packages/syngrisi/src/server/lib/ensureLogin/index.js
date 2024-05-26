e0ff5a66dc17468bccec5ee6c304b858892a67da
Viktor Silakov <23633060+viktor-silakov@users.noreply.github.com>
2024-05-26 23:06:16 +0400

    js -> ts 1.10 - enshure login

--- packages/syngrisi/src/server/lib/ensureLogin/index.js
+++ packages/syngrisi/src/server/lib/ensureLogin/index.js
@@ -2,7 +2,7 @@ module.exports.ensureLoggedIn = require('../../../../dist/src/server/lib/ensureL
 
 module.exports.ensureApiKey = require('../../../../dist/src/server/lib/ensureLogin/ensureLoggedIn').ensureApiKey;
 module.exports.ensureLoggedInOrApiKey = require('../../../../dist/src/server/lib/ensureLogin/ensureLoggedIn').ensureLoggedInOrApiKey;
-module.exports.ensureLoggedOut = require('../../../../dist/src/server/lib/ensureLogin/ensureLoggedOut').ensureLoggedOut;
+// module.exports.ensureLoggedOut = require('../../../../dist/src/server/lib/ensureLogin/ensureLoggedOut').ensureLoggedOut;
 
 
 // module.exports.ensureLoggedIn = require('./ensureLoggedIn').ensureLoggedIn;
