69db1b3892d57a3b794abeecbf8f681a1096b46e
Viktor Silakov <23633060+viktor-silakov@users.noreply.github.com>
2024-05-28 11:27:17 +0400

    js -> ts 2.3 - controllers, routes

--- packages/syngrisi/src/server/routes/v1/app.route.js
+++
@@ -1,9 +0,0 @@
-// const express = require('express');
-// const appController = require('../../controllers/app.controller');
-
-// const router = express.Router();
-
-// router.get('/info', appController.info);
-// router.get('/', appController.get);
-
-// module.exports = router;
--- packages/syngrisi/test/tests/e2e/example.spec.ts
+++ packages/syngrisi/test/tests/e2e/example.spec.ts
@@ -1,16 +1,42 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // import { expect } from '@playwright/test';
 import { test } from '../../src/fixtures/base';
 
-test('has title @tag1', async ({ page, server, baseUrl, log }) => {
-  
+test('has title @tag1', async ({ page, server, baseUrl, log, request }) => {
+
+  // Define the callback function
+  function logRequestParams(request: any) {
+    const url = request.url();
+    const method = request.method();
+    const headers = request.headers();
+    const postData = request.postData();
+
+    console.log(`✅ Request made to: ${url}`);
+    console.log(`Method: ${method}`);
+    console.log(`Headers: ${JSON.stringify(headers, null, 2)}`);
+    if (postData) {
+      console.log(`Post Data: ${postData}`);
+    }
+  }
+
+  // Listen for requests
+  page.on('request', request => {
+    if (request.url().includes('/v1/')) {
+      logRequestParams(request);
+    }
+  });
+
+
   await page.goto(baseUrl);
 
-    log.debug("DEBUG");
-    log.info("INFO");
-    log.warn("WARN");
-    log.error("ERROR");
-    log.info('Test');
-    
+  await page.pause();
+
+  // log.debug("DEBUG");
+  // log.info("INFO");
+  // log.warn("WARN");
+  // log.error("ERROR");
+  // log.info('Test');
+
 
   // await locator('#test');
   // await page.pause();
@@ -21,12 +47,12 @@ test('has title @tag1', async ({ page, server, baseUrl, log }) => {
 });
 
 
-test('test2 @tag2', async ({ page, server , baseUrl }) => {
-    console.log('test2');
-    await page.goto(baseUrl);
-    // await page.pause();
-});
+// test('test2 @tag2', async ({ page, server , baseUrl }) => {
+//     console.log('test2');
+//     await page.goto(baseUrl);
+//     // await page.pause();
+// });
 
-test('test3 @no_server', async ({ server, baseUrl }) => {
-  console.log('test3');
-});
+// test('test3 @no_server', async ({ server, baseUrl }) => {
+//   console.log('test3');
+// });
