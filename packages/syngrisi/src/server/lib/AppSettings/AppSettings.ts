b62309f8e0006bb00da827f335e26f778262a4c3
Viktar <1105714@gmail.com>
2024-05-30 21:47:05 +0400

    refactoring: update appSettings, decomse auth controller

--- packages/syngrisi/src/server/lib/AppSettings/AppSettings.ts
+++ packages/syngrisi/src/server/lib/AppSettings/AppSettings.ts
@@ -10,49 +10,75 @@ class AppSettings {
 
     constructor() {
         this.model = AppSettingsModel;
-        (async () => {
-            this.cache = await this.model.find().lean().exec();
-        })();
+        this.cache = null; // Initialize cache as null to indicate uninitialized state
     }
 
-    count(): Promise<number> {
+    async init() {
+        this.cache = await this.model.find().lean().exec();
+        return this;
+    }
+
+    private ensureInitialized() {
+        if (!this.cache) {
+            throw new Error('AppSettings is not initialized. Please call init() before using this method.');
+        }
+    }
+
+    async count(): Promise<number> {
+        this.ensureInitialized();
         return this.model.countDocuments().exec();
     }
 
     async loadInitialFromFile(): Promise<void> {
+        this.ensureInitialized();
         const settings = initialAppSettings;
         await this.model.insertMany(settings);
         this.cache = settings;
     }
 
     async get(name: string): Promise<any> {
-        return this.cache?.find((x: any) => x.name === name) 
+        this.ensureInitialized();
+        return this.cache.find((x: any) => x.name === name) 
             || (this.model.findOne({ name }).exec());
     }
 
     async set(name: string, value: any): Promise<void> {
+        this.ensureInitialized();
         const item = await this.model.findOneAndUpdate({ name }, { value });
         await item.save();
-        this.cache.find((x: any) => x.name === name)['value'] = value;
+        const cachedItem = this.cache.find((x: any) => x.name === name);
+        if (cachedItem) {
+            cachedItem['value'] = value;
+        }
     }
 
     async enable(name: string): Promise<void> {
+        this.ensureInitialized();
         const item = await this.model.findOneAndUpdate({ name }, { enabled: true });
         await item.save();
-        this.cache.find((x: any) => x.name === name)['enabled'] = true;
+        const cachedItem = this.cache.find((x: any) => x.name === name);
+        if (cachedItem) {
+            cachedItem['enabled'] = true;
+        }
     }
 
     async disable(name: string): Promise<void> {
+        this.ensureInitialized();
         const item = await this.model.findOneAndUpdate({ name }, { enabled: false });
         await item.save();
-        this.cache.find((x: any) => x.name === name)['enabled'] = false;
+        const cachedItem = this.cache.find((x: any) => x.name === name);
+        if (cachedItem) {
+            cachedItem['enabled'] = false;
+        }
     }
 
     async isAuthEnabled(): Promise<boolean> {
+        this.ensureInitialized();
         return process.env.SYNGRISI_AUTH === '1' || (await this.get('authentication'))?.value === 'true';
     }
 
     async isFirstRun(): Promise<boolean> {
+        this.ensureInitialized();
         return (await this.get('first_run'))?.value === 'true';
     }
 }
---
+++ packages/syngrisi/src/server/services/auth.service.ts
@@ -0,0 +1,92 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import hasha from 'hasha';
+import uuidAPIKey from 'uuid-apikey';
+import { User } from '../models';
+import log from "../lib/logger";
+import { User as IUser } from '../../types/User';
+
+function getApiKey(): string {
+    return uuidAPIKey.create().apiKey;
+}
+
+const generateApiKey = async (username: string): Promise<string> => {
+    const logOpts = {
+        user: username,
+        scope: 'apikey',
+        msgType: 'GENERATE_API'
+    };
+
+    const apiKey = getApiKey();
+    log.debug(
+        `generate API Key for user: '${username}'`,
+        logOpts
+    );
+    const hash = hasha(apiKey);
+
+    const user: IUser | null = await User.findOne({ username });
+    if (!user) throw new Error(`cannot find the user with username: '${username}'`);
+
+    user.apiKey = hash;
+    await user.save();
+    return apiKey;
+};
+
+const changeUserPassword = async (username: string, currentPassword: string, newPassword: string): Promise<void> => {
+    const logOpts = {
+        scope: 'changePassword',
+        msgType: 'CHANGE_PASSWORD',
+        itemType: 'user',
+        ref: username,
+    };
+
+    log.debug(`change password for '${username}', params: '${JSON.stringify({ currentPassword, newPassword })}'`, logOpts);
+
+    const user: IUser | null = await User.findOne({ username });
+    if (!user) {
+        log.error('user is not logged in', logOpts);
+        throw new Error('user is not logged in');
+    }
+
+    try {
+        await user.changePassword(currentPassword, newPassword);
+    } catch (e: any) {
+        log.error(e.stack || e.toString(), logOpts);
+        throw new Error(e.toString());
+    }
+
+    log.debug(`password was successfully changed for user: ${username}`, logOpts);
+};
+
+const changePasswordFirstRun = async (newPassword: string): Promise<void> => {
+    const logOpts = {
+        scope: 'changePasswordFirstRun',
+        msgType: 'CHANGE_PASSWORD_FIRST_RUN',
+        itemType: 'user',
+        ref: 'Administrator',
+    };
+
+    const AppSettings = (global as any).AppSettings;
+
+    if ((await AppSettings.isAuthEnabled()) && ((await AppSettings.isFirstRun()))) {
+        log.debug(`first run, change password for default 'Administrator', params: '${JSON.stringify({ newPassword })}'`, logOpts);
+        const user = await User.findOne({ username: 'Administrator' }).exec();
+        logOpts.ref = String(user?.username);
+
+        // @ts-ignore
+        await user.setPassword(newPassword);
+        // @ts-ignore
+        await user.save();
+        log.debug('password was successfully changed for default Administrator', logOpts);
+        await AppSettings.set('first_run', false);
+    } else {
+        log.error(`trying to use first run API with no first run state, auth: '${await AppSettings.isAuthEnabled()}', global settings: '${(await AppSettings.get('first_run'))}'`, logOpts);
+        throw new Error('forbidden');
+    }
+};
+
+export {
+    generateApiKey,
+    changeUserPassword,
+    changePasswordFirstRun,
+};
