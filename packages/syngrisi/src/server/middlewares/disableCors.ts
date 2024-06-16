30668d3e25eda2accb05406f62e8bb7c96ed2307
Viktor Silakov <23633060+viktor-silakov@users.noreply.github.com>
2024-06-16 08:42:52 +0400

    fix vite - cors problems

--- packages/syngrisi/src/server/middlewares/disableCors.ts
+++ packages/syngrisi/src/server/middlewares/disableCors.ts
@@ -4,10 +4,11 @@ import { config } from '@config';
 
 // disable CORS for development purposes
 export const disableCors = (req: Request, res: Response, next: NextFunction): void => {
-    if (config.disableCors) return next();
-
+    if (!config.disableCors) return next();
+    
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -18,6 +19,12 @@ export const disableCors = (req: Request, res: Response, next: NextFunction): vo
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
 
     // Pass to next layer of middleware
     return next();
