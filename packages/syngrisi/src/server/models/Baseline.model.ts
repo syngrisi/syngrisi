97f22cdf78af23acb6aa925c3d90200f4526c4ab
Viktor Silakov <23633060+viktor-silakov@users.noreply.github.com>
2024-06-21 12:25:29 +0400

    rename models

---
+++ packages/syngrisi/src/server/models/Baseline.model.ts
@@ -0,0 +1,98 @@
+import mongoose, { Schema, Document, Model } from 'mongoose';
+import { toJSON, paginate } from './plugins';
+import { PluginExtededModel } from './plugins/utils';
+
+export interface BaselineDocument extends Document {
+    snapshootId?: Schema.Types.ObjectId;
+    name: { [key: string]: string | boolean | number };
+    app: Schema.Types.ObjectId;
+    branch?: string;
+    browserName?: string;
+    browserVersion?: string;
+    browserFullVersion?: string;
+    viewport?: string;
+    os?: string;
+    markedAs?: 'bug' | 'accepted';
+    lastMarkedDate?: Date;
+    createdDate?: Date;
+    updatedDate?: Date;
+    markedById?: Schema.Types.ObjectId;
+    markedByUsername?: string;
+    ignoreRegions?: string;
+    boundRegions?: string;
+    matchType?: 'antialiasing' | 'nothing' | 'colors';
+    meta?: Record<string, unknown>;
+}
+
+const BaselineSchema: Schema<BaselineDocument> = new Schema({
+    snapshootId: {
+        type: Schema.Types.ObjectId,
+    },
+    name: {
+        type: String,
+        required: 'VRSBaselineSchema: the name of the snapshoot entity is empty',
+    },
+    app: {
+        type: Schema.Types.ObjectId,
+        ref: 'VRSApp',
+        required: 'VRSBaselineSchema: the app field is empty',
+    },
+    branch: {
+        type: String,
+    },
+    browserName: {
+        type: String,
+    },
+    browserVersion: {
+        type: String,
+    },
+    browserFullVersion: {
+        type: String,
+    },
+    viewport: {
+        type: String,
+    },
+    os: {
+        type: String,
+    },
+    markedAs: {
+        type: String,
+        enum: ['bug', 'accepted'],
+    },
+    lastMarkedDate: {
+        type: Date,
+    },
+    createdDate: {
+        type: Date,
+    },
+    updatedDate: {
+        type: Date,
+    },
+    markedById: {
+        type: Schema.Types.ObjectId,
+        ref: 'VRSUser',
+    },
+    markedByUsername: {
+        type: String,
+    },
+    ignoreRegions: {
+        type: String,
+    },
+    boundRegions: {
+        type: String,
+    },
+    matchType: {
+        type: String,
+        enum: ['antialiasing', 'nothing', 'colors'],
+    },
+    meta: {
+        type: Object,
+    },
+});
+
+BaselineSchema.plugin(toJSON);
+BaselineSchema.plugin(paginate);
+
+const Baseline: Model<BaselineDocument> = mongoose.model<BaselineDocument>('VRSBaseline', BaselineSchema);
+export default Baseline as PluginExtededModel<BaselineDocument>;
+
---
+++ packages/syngrisi/src/server/models/Run.model.ts
@@ -0,0 +1,55 @@
+
+import mongoose, { Schema, Document, Model } from 'mongoose';
+import { toJSON, paginate } from './plugins';
+import { PluginExtededModel } from './plugins/utils';
+
+export interface RunDocument extends Document {
+    name: { [key: string]: string | boolean | number };
+    app: Schema.Types.ObjectId;
+    ident: { [key: string]: string | boolean | number };
+    description?: string;
+    updatedDate?: Date;
+    createdDate?: Date;
+    parameters?: string[];
+    meta?: Record<string, unknown>;
+}
+
+const RunSchema: Schema<RunDocument> = new Schema({
+    name: {
+        type: String,
+        required: 'RunSchema: the run name cannot be empty',
+    },
+    app: {
+        type: Schema.Types.ObjectId,
+        ref: 'VRSApp',
+        required: 'RunSchema: the app field is empty',
+    },
+    ident: {
+        type: String,
+        unique: true,
+        required: 'RunSchema: the run ident run cannot be empty',
+    },
+    description: {
+        type: String,
+    },
+    updatedDate: {
+        type: Date,
+        default: Date.now,
+    },
+    createdDate: {
+        type: Date,
+    },
+    parameters: {
+        type: [String],
+    },
+    meta: {
+        type: Object,
+    },
+});
+
+RunSchema.plugin(paginate);
+RunSchema.plugin(toJSON);
+
+const Run: Model<RunDocument> = mongoose.model<RunDocument>('VRSRun', RunSchema);
+export default Run as PluginExtededModel<RunDocument>;
+
