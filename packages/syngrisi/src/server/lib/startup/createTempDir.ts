55cabff167fb0361e424f97b362dc0eef759a785
Viktar <1105714@gmail.com>
2024-06-30 08:36:41 +0400

    tmpdir to config

--- packages/syngrisi/src/server/lib/startup/createTempDir.ts
+++ packages/syngrisi/src/server/lib/startup/createTempDir.ts
@@ -1,7 +1,8 @@
 import fs from 'fs';
+import { config } from '@config'
 
 export function createTempDir(): void {
-    const dir = '.tmp';
+    const dir = config.tmpDir;
     if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
     }
