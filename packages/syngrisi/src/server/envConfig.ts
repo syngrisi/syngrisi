147eb185b759590dcbc98d62eed6ae6e87a679f9
Viktor Silakov <23633060+viktor-silakov@users.noreply.github.com>
2025-01-19 14:49:05 +0400

    Refactor environment configuration in envConfig.ts to standardize default values for environment variables. This change ensures consistency by replacing 'devDefault' with 'default' for all variables, enhancing clarity and maintainability of the configuration.

--- packages/syngrisi/src/server/envConfig.ts
+++ packages/syngrisi/src/server/envConfig.ts
@@ -10,31 +10,31 @@ if (!process.env.NODE_ENV) {
 }
 
 export const env = cleanEnv(process.env, {
-  NODE_ENV: str({ choices: ['development', 'production', 'test']}),
-  SYNGRISI_DB_URI: str({ devDefault: 'mongodb://127.0.0.1:27017/SyngrisiDb' }),
-  SYNGRISI_APP_PORT: port({ devDefault: 3000 }),
-  SYNGRISI_IMAGES_PATH: str({ devDefault: "./.snapshots-images/" }),
-  SYNGRISI_TMP_DIR: str({ default: path.join(process.cwd(), '.tmp') }),
-  SYNGRISI_HTTP_LOG: bool({ devDefault: false }),
-  SYNGRISI_COVERAGE: bool({ devDefault: false }),
+    NODE_ENV: str({ choices: ['development', 'production', 'test'] }),
+    SYNGRISI_DB_URI: str({ default: 'mongodb://127.0.0.1:27017/SyngrisiDb' }),
+    SYNGRISI_APP_PORT: port({ default: 3000 }),
+    SYNGRISI_IMAGES_PATH: str({ default: "./.snapshots-images/" }),
+    SYNGRISI_TMP_DIR: str({ default: path.join(process.cwd(), '.tmp') }),
+    SYNGRISI_HTTP_LOG: bool({ default: false }),
+    SYNGRISI_COVERAGE: bool({ default: false }),
 
-  SYNGRISI_HOSTNAME: host({ devDefault: 'localhost' }),
+    SYNGRISI_HOSTNAME: host({ default: 'localhost' }),
 
-  SYNGRISI_AUTH: bool({ default: true }),
-  SYNGRISI_TEST_MODE: bool({ default: false }),
-  SYNGRISI_DISABLE_FIRST_RUN: bool({ default: false }),
+    SYNGRISI_AUTH: bool({ default: true }),
+    SYNGRISI_TEST_MODE: bool({ default: false }),
+    SYNGRISI_DISABLE_FIRST_RUN: bool({ default: false }),
 
-  MONGODB_ROOT_USERNAME: str({ default: '' }),
-  MONGODB_ROOT_PASSWORD: str({ default: '' }),
-  LOGLEVEL: str({ choices: ['error', 'warn', 'info', 'verbose', 'debug', 'silly'], default: 'debug' }),
-  SYNGRISI_PAGINATION_SIZE: num({ default: 50 }),
-  SYNGRISI_DISABLE_DEV_CORS: bool({ default: false, devDefault: true }),
-  SYNGRISI_SESSION_STORE_KEY: str({ default: crypto.randomBytes(64).toString('hex') }),
-  SYNGRISI_LOG_LEVEL: str({ default: 'debug' }),
+    MONGODB_ROOT_USERNAME: str({ default: '' }),
+    MONGODB_ROOT_PASSWORD: str({ default: '' }),
+    LOGLEVEL: str({ choices: ['error', 'warn', 'info', 'verbose', 'debug', 'silly'], default: 'debug' }),
+    SYNGRISI_PAGINATION_SIZE: num({ default: 50 }),
+    SYNGRISI_DISABLE_DEV_CORS: bool({ default: true, devDefault: true }),
+    SYNGRISI_SESSION_STORE_KEY: str({ default: crypto.randomBytes(64).toString('hex') }),
+    SYNGRISI_LOG_LEVEL: str({ default: 'debug' }),
 
-  // trunk features
-  SYNGRISI_TRUNK_FEATURE_AI_SEVERITY: bool({ default: false }),
-  SYNGRISI_AI_KEY: str({ default: '' }),
-  OPENAI_API_BASE_URL: str({ default: 'https://api.openai.com/v1' }),
-  OPENAI_API_KEY: str({ default: '' }),
+    // trunk features
+    SYNGRISI_TRUNK_FEATURE_AI_SEVERITY: bool({ default: false }),
+    SYNGRISI_AI_KEY: str({ default: '' }),
+    OPENAI_API_BASE_URL: str({ default: 'https://api.openai.com/v1' }),
+    OPENAI_API_KEY: str({ default: '' }),
 });
